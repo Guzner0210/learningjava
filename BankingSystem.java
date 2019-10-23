645f639de3681d3e0eb64fd6262b35b75976ec3e
Duc Bui <buixuanduc2930@gmail.com>
2019-10-23 17:10:37 -0400

    Added addRecord()

--- BankingSystem.java
+++ BankingSystem.java
@@ -20,7 +20,27 @@ class BankingSystem {
   }
 
   public static void addRecord() {
-    Account newAccount = new Account(1234, "Toni", "Bui", 900.00);
+    System.out.println("Enter your account number: ");
+    Scanner _accountNumber = new Scanner(System.in);
+    int accountNumber = _accountNumber.nextInt();
+
+    System.out.println("Enter your first name: ");
+    Scanner _firstName = new Scanner(System.in);
+    String firstName = _firstName.nextLine();
+
+    System.out.println("Enter your last name: ");
+    Scanner _lastName = new Scanner(System.in);
+    String lastName = _lastName.nextLine();
+
+    System.out.println("Enter your balance: ");
+    Scanner _balance = new Scanner(System.in);
+    double balance = _balance.nextDouble();
+
+    Account newAccount = new Account(accountNumber, firstName, lastName, balance);
+    System.out.println("This is your new account: ");
+    System.out.println(newAccount.accountNumber);
+    System.out.println(newAccount.firstName);
+    System.out.println(newAccount.lastName);
     System.out.println(newAccount.balance);
   }
 
