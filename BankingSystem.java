320041247cfdd9587fc17dab2d5b292f6adba4bb
Duc Bui <buixuanduc2930@gmail.com>
2019-10-23 17:04:34 -0400

    Added a class of account

--- BankingSystem.java
+++ BankingSystem.java
@@ -5,6 +5,25 @@ class BankingSystem {
     decide();
   }
 
+  public static class Account {
+    int accountNumber;
+    String firstName;
+    String lastName;
+    double balance;
+
+    public Account(int _accountNumber, String _firstName, String _lastName, double _balance) {
+      accountNumber = _accountNumber;
+      firstName = _firstName;
+      lastName = _lastName;
+      balance = _balance;
+    }
+  }
+
+  public static void addRecord() {
+    Account newAccount = new Account(1234, "Toni", "Bui", 900.00);
+    System.out.println(newAccount.balance);
+  }
+
   public static void decide() {
     System.out.println("What do you want to do?");
     System.out.println("1> Add a record");
@@ -15,13 +34,13 @@ class BankingSystem {
     Scanner myObj = new Scanner(System.in);
     int ans = myObj.nextInt();
     if (ans == 1) {
-      // Add a record
+      addRecord();
     } else if (ans == 2) {
       // Show records
     } else if (ans == 3) {
       // Edit records
     } else {
-      
+
     }
   }
 }
