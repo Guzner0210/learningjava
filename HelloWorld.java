2e9fbcf55bf8e8915efb0993e02988b65dc96af2
Duc Bui <buixuanduc2930@gmail.com>
2019-10-23 15:16:53 -0400

    Started the project!

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+class HelloWorld {
+  public static void main(String[] args) {
+    decide();
+  }
+
+  public static void decide() {
+    System.out.println("What do you want to do?");
+    System.out.println("1> Add a record");
+    System.out.println("2> Show records");
+    System.out.println("3> Edit records");
+    System.out.println("4> Close program");
+  }
+}
